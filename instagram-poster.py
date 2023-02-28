e54864f50efae739c09deaaa7f4b47f28d2c2f2f
lorossi <mail@lorenzoros.si>
2023-02-28 17:28:44 +0100

    added logging

--- instagram-poster.py
+++ instagram-poster.py
@@ -1,24 +1,37 @@
 import schedule
 import time
+import logging
 
 from modules.instagram import Instagram
 from modules.saint_factory import SaintFactory
 
 
 def publish():
+    logging.info("Publishing post")
     s = SaintFactory()
     saint = s.generateSaint()
     caption = saint.bio + "\n\n#santodelgiorno #santino_quotidiano"
     i = Instagram()
     i.uploadImage(image_path=saint.image_path, image_caption=caption)
+    logging.info("Post published")
 
 
 def main():
+    logging.info("Starting instagram poster")
     schedule.every().day.at("08:00").do(publish)
+    logging.info("Posts scheduled")
     while True:
         schedule.run_pending()
         time.sleep(1)
 
 
 if __name__ == "__main__":
+    filename = __file__.replace(".py", ".log")
+    logging.basicConfig(
+        level=logging.INFO,
+        format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
+        filename=filename,
+        filemode="w",
+    )
+
     main()
