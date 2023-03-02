cb44e5698520591c712db5519357486a9c7db338
lorossi <mail@lorenzoros.si>
2023-03-02 22:48:12 +0100

    created scheduler class for automated scheduling

--- modules/instagram_poster.py
+++ modules/instagram_poster.py
@@ -1,126 +1,41 @@
 """Module containing the instagram poster class."""
 from __future__ import annotations
 
-import datetime
 import logging
-import time
-
-import pytz
-import schedule
-import toml
 
 from modules.instagram import Instagram
 from modules.saint_factory import SaintFactory
 
+from .scheduler import Scheduler
 
-class InstagramPoster:
+
+class InstagramPoster(Scheduler):
     """Class handling the logic of the instagram poster."""
 
-    _timezone: pytz.timezone = pytz.timezone("Europe/Rome")
-
-    def __init__(self, settings_path: str = "settings.toml") -> InstagramPoster:
+    def __init__(self) -> InstagramPoster:
         """Initialize the poster."""
         logging.info("Initializing instagram poster")
+        super().__init__()
         self._factory = SaintFactory()
         self._instagram = Instagram()
-        self._settings = self._loadSettings(settings_path)
-        self._post_time = self._loadPostTime()
-
-    def upload(self) -> None:
-        """Publish a post."""
-        if not self._tryLogin():
-            return
-
-        self._tryUpload()
-        logging.info("Upload done")
-        next_run = schedule.next_run().isoformat()
-        logging.info(f"Next post scheduled for {next_run}")
+        self._post_time = self.loadScheduleTime("post_time")
 
     def start(self) -> None:
         """Loop the poster."""
-        logging.info("Starting instagram poster")
+        logging.info("Starting instagram poster loop")
+        super().start("post_time", self.upload)
 
-        post_time_str = self._post_time.strftime("%H:%M")
-        schedule.every().day.at(post_time_str).do(self.upload)
-        next_run = schedule.next_run().isoformat()
-        logging.info(f"Posts scheduled for {next_run}")
-        while True:
-            try:
-                schedule.run_pending()
-                time.sleep(1)
-            except KeyboardInterrupt:
-                logging.warning("Keyboard interrupt called. Exiting...")
-                return
+    def upload(self) -> None:
+        """Publish a post."""
+        if not self.tryFunction(self._instagram.login):
+            return
 
-    def _loadSettings(self, settings_path: str) -> dict:
-        """Load settings from a toml file.
+        if not self.tryFunction(self._uploadImage):
+            return
 
-        Args:
-            settings_path (str): Path to the settings file
+        logging.info("Upload done")
 
-        Returns:
-            dict: Settings
-        """
-        logging.info("Loading settings")
-        with open(settings_path, "r") as f:
-            settings = toml.load(f)[self.__class__.__name__]
-        return settings
-
-    def _loadPostTime(self) -> datetime.time:
-        """Load the time at which the bot posts the saint.
-
-        Returns:
-            datetime.time: Post time
-        """
-        return datetime.datetime.strptime(self._settings["post_time"], "%H:%M").replace(
-            tzinfo=self._timezone
-        )
-
-    def _tryLogin(self) -> bool:
-        """Try to login to Instagram.
-
-        Returns:
-            bool: True if the login was successful, False otherwise
-        """
-        logging.info("Logging into Instagram...")
-        tries = 0
-        while tries < self._settings["max_tries"]:
-            try:
-                self._instagram.login()
-                logging.info("Logged")
-                return True
-            except Exception as e:
-                logging.error(f"Error while logging in: {e}")
-                tries += 1
-                logging.info(
-                    f"Retrying in 10 seconds... ({tries}/"
-                    f"{self._settings['max_tries']})"
-                )
-                time.sleep(10)
-
-        logging.error("Aborting login after failed amounts.")
-        return False
-
-    def _tryUpload(self) -> bool:
-        tries = 0
-        logging.info("Posting image....")
-        while tries < self._settings["max_tries"]:
-            try:
-                saint = self._factory.generateSaint()
-                caption = saint.bio + "\n\n#santodelgiorno #santinoquotidiano"
-                self._instagram.uploadImage(
-                    image_path=saint.image_path, image_caption=caption
-                )
-                logging.info("Image posted")
-                return True
-            except Exception as e:
-                logging.error(f"Error while posting: {e}")
-                tries += 1
-                logging.info(
-                    f"Retrying in 10 seconds... ({tries}/"
-                    f"{self._settings['max_tries']})"
-                )
-                time.sleep(10)
-
-        logging.error("Aborting post after failed amounts.")
-        return False
+    def _uploadImage(self) -> None:
+        saint = self._factory.generateSaint()
+        caption = saint.bio + "\n\n#santodelgiorno #santinoquotidiano"
+        self._instagram.uploadImage(image_path=saint.image_path, image_caption=caption)
---
+++ modules/scheduler.py
@@ -0,0 +1,103 @@
+from __future__ import annotations
+
+import logging
+from datetime import datetime
+from time import sleep
+
+import pytz
+import schedule
+import toml
+
+
+class Scheduler:
+    """Class handling the logic of the scheduler."""
+
+    _timezone: pytz.timezone = pytz.timezone("Europe/Rome")
+
+    def __init__(self) -> Scheduler:
+        """Initialize the scheduler."""
+        self._settings = self._loadSettings()
+
+    def _loadSettings(self, path: str = "settings.toml", key: str = None) -> dict:
+        """Load settings from a toml file.
+
+        Args:
+            settings_path (str): Path to the settings file
+
+        Returns:
+            dict: Settings
+        """
+        logging.info("Loading settings")
+        with open(path, "r") as settings_file:
+            settings = toml.load(settings_file)
+
+        logging.info("Settings loaded")
+
+        if key is None and self.__class__.__name__ in settings:
+            return settings[self.__class__.__name__]
+
+        if key is not None:
+            return settings[key]
+
+        return settings
+
+    def loadScheduleTime(self, key: str) -> datetime:
+        """Load the time the image was generated.
+
+        Returns:
+            datetime: Time the image was generated.
+        """
+        return datetime.strptime(self._settings[key], "%H:%M").replace(
+            tzinfo=self._timezone
+        )
+
+    def _schedule(self, key: str, function: callable) -> None:
+        """Schedule a function to run at a specific time once a day.
+
+        Args:
+            key (str): key containing the run time in format %H:%M
+            function (callable): function to run
+        """
+        schedule_time = self.loadScheduleTime(key)
+        schedule_time_str = schedule_time.strftime("%H:%M")
+        schedule.every().day.at(schedule_time_str).do(function)
+
+    def tryFunction(self, function: callable, retry_delay: int = 10) -> bool:
+        """Try to run a function."""
+        tries = 0
+        max_tries = self._settings["max_tries"]
+        while tries < max_tries:
+            try:
+                function()
+                return True
+            except Exception as e:
+                logging.error(f"Error while running function: {e}")
+                tries += 1
+                logging.info(
+                    f"Trying again in {retry_delay} second ({tries}/{max_tries})"
+                )
+                sleep(retry_delay)
+
+        logging.error("Max tries reached. Exiting...")
+        return False
+
+    def start(self, key: str, function: callable) -> None:
+        """Loop the creator."""
+        self._schedule(key, function)
+        logging.info(f"Function {function.__name__} scheduled for {self.next_run}")
+        while True:
+            try:
+                schedule.run_pending()
+                sleep(1)
+            except KeyboardInterrupt:
+                logging.warning("Keyboard interrupt called. Exiting...")
+                return
+
+    @property
+    def next_run(self) -> str:
+        """Get the next run time.
+
+        Returns:
+            str: run time, iso format
+        """
+        return schedule.next_run().isoformat()
