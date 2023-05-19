64bd3a2adffb31e26c650e8c9eda7e326ac6f20b
lorossi <mail@lorenzoros.si>
2023-05-19 15:47:57 +0200

    re-enabled instagram posting

--- launcher.sh
+++ launcher.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 cd /home/pi/scripts/saint-of-the-day
 venv/bin/python3 saint-handler.py &
-# venv/bin/python3 instagram-handler.py &
+venv/bin/python3 instagram-handler.py &
 venv/bin/python3 telegram-handler.py &
