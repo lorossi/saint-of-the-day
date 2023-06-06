9daf48920dd1224c862ab3debc3bbe92c5cb51e7
lorossi <mail@lorenzoros.si>
2023-06-06 15:35:26 +0200

    updated launcher

--- launcher.sh
+++ launcher.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
-cd /home/pi/scripts/saint-of-the-day
+# change to the script directory
+cd "$( dirname "${BASH_SOURCE[0]}" )"
+# launch the scripts in the virtual environment
 venv/bin/python3 saint-handler.py &
 venv/bin/python3 instagram-handler.py &
 venv/bin/python3 telegram-handler.py &
